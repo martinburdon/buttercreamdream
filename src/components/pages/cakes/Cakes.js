0ff5575893f1b26c51ec4beef0a62a9f5a5fff85
martinburdon <martin@martinburdon.co.uk>
2018-07-21 19:01:15 +0100

    Add pics to cakes page

--- src/components/pages/cakes/Cakes.js
+++ src/components/pages/cakes/Cakes.js
@@ -3,11 +3,19 @@ import { NavLink } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import siteConfig from 'config/site-config.js';
 import { formatCakeUrl, formatCakeTitle } from 'helpers.js';
+import Photo from 'components/gallery/Photo.js';
 
-const getLinks = ({ title }, i) => {
+const getLinks = ({ thumb, title }, i) => {
   const fullTitle = formatCakeTitle(title);
   const url = formatCakeUrl(title);
-  return <li key={i}><NavLink to={url}>{fullTitle}</NavLink></li>;
+  return (
+    <li key={i}>
+      <NavLink to={url}>
+        <Photo thumb={thumb} title={title} />
+        <span className="title">{fullTitle}</span>
+      </NavLink>
+    </li>
+  );
 };
 
 export default () => {
@@ -19,7 +27,7 @@ export default () => {
         <title>Cakes in Sunderland | {siteConfig.meta.title}</title>
       </Helmet>
       <h1>Cakes in Sunderland</h1>
-      <ul>{links}</ul>
+      <ul className="cakes-grid">{links}</ul>
     </div>
   )
 };
