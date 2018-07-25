545e0969b7b78c04f3e70e871b7cdbaee0dd5462
martinburdon <martin@martinburdon.co.uk>
2018-07-25 13:58:32 +0100

    Use webp images if available

--- src/components/pages/GalleryPage.js
+++ src/components/pages/GalleryPage.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Gallery from 'components/gallery/Gallery.js';
-import imagesGallery from 'config/images-gallery.json';
 
 export default () => (
   <div>
-    <Gallery images={imagesGallery} />
+    <Gallery images={[]} />
   </div>
 );
