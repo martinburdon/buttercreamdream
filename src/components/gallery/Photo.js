8e5a5cc32c93ab060b99490ec88c45c631819b34
Martin Burdon <martin@martinburdon.co.uk>
2019-10-15 21:36:21 +0100

    Add birthday cakes to sitemap

--- src/components/gallery/Photo.js
+++ src/components/gallery/Photo.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default ({ placeholder, thumb, title }) => (
   <gallery-item>
-    <img className="lazy" src={placeholder} data-src={thumb} alt={title} />
+    <img src={thumb} alt={title} />
   </gallery-item>
 );
