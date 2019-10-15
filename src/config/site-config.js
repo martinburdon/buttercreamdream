44fbf29fec750d76b6f9bf0b23c5da2412f221c6
Martin Burdon <martin@martinburdon.co.uk>
2019-10-15 20:59:01 +0100

    Update SEO content

--- src/config/site-config.js
+++ src/config/site-config.js
@@ -5,8 +5,8 @@ export default {
   images,
   cakePages,
   meta: {
-    description: 'Cakes and cupcakes for all occasions. We can make cakes for all occasions such as cupcakes, birthday cakes, childrens birthday cakes, anniversary cakes, christening cakes, wedding cakes, seasonal cakes',
-    title: 'Cupcakes and cakes in Sunderland',
+    description: 'Cakes and cupcakes for all occasions. I can make cakes for all occasions such as cupcakes, birthday cakes, childrens birthday cakes, anniversary cakes, christening cakes, wedding cakes, seasonal cakes.',
+    title: 'Buttercream Dream | Cake maker in Sunderland',
     keywords: 'cakes, cupcakes, birthdays, childrens cakes, anniversary, christening, wedding, seasonal, easter, christmas, teacher, cake pops, cakes in sunderland, cakes in the north east, custom cakes, birthday cakes'
   }
 };
